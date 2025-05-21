cc1123dcb91fb86e88b63bec65a62bd6e61a1273
안병진 <bbyungs@anbyeongjin-ui-Macmini.local>
2025-05-21 20:22:55 +0900

    #6.6 Stateful Navigation part1

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,3 +1,4 @@
+import 'package:clone/features/main_navigation/stf_screen.dart';
 import 'package:clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -13,21 +14,27 @@ class _MainNavigationState extends State<MainNavigation> {
   int _selectedIndex = 0;
 
   final screens = [
-    Center(
-      child: Text('Home'),
-    ),
-    Center(
-      child: Text('Search'),
-    ),
-    Center(
-      child: Text('Search'),
-    ),
-    Center(
-      child: Text('Search'),
-    ),
-    Center(
-      child: Text('Search'),
-    ),
+    // 각자 key를 부여하면 각 화면을 서로 다른 widget인것처럼 렌더링 함
+    StfScreen(key: GlobalKey()),
+    StfScreen(key: GlobalKey()),
+    Container(),
+    StfScreen(key: GlobalKey()),
+    StfScreen(key: GlobalKey()),
+    // Center(
+    //   child: Text('Home'),
+    // ),
+    // Center(
+    //   child: Text('Search'),
+    // ),
+    // Center(
+    //   child: Text('Search'),
+    // ),
+    // Center(
+    //   child: Text('Search'),
+    // ),
+    // Center(
+    //   child: Text('Search'),
+    // ),
   ];
 
   void _onTap(int index) {
@@ -39,33 +46,40 @@ class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: screens[_selectedIndex],
+      body: screens.elementAt(_selectedIndex),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             NavTab(
-                text: 'Home',
-                isSelected: _selectedIndex == 0,
-                icon: FontAwesomeIcons.house,
-                onTap: () => _onTap(0)),
+              text: 'Home',
+              isSelected: _selectedIndex == 0,
+              icon: FontAwesomeIcons.house,
+              onTap: () => _onTap(0),
+              selectedIcon: FontAwesomeIcons.house,
+            ),
             NavTab(
               text: 'Discover',
               isSelected: _selectedIndex == 1,
-              icon: FontAwesomeIcons.magnifyingGlass,
+              icon: FontAwesomeIcons.compass,
               onTap: () => _onTap(1),
+              selectedIcon: FontAwesomeIcons.solidCompass,
             ),
             NavTab(
               text: 'Inbox',
               isSelected: _selectedIndex == 3,
               icon: FontAwesomeIcons.message,
               onTap: () => _onTap(3),
+              selectedIcon: FontAwesomeIcons.solidMessage,
             ),
             NavTab(
               text: 'Profile',
               isSelected: _selectedIndex == 4,
               icon: FontAwesomeIcons.user,
               onTap: () => _onTap(4),
+              selectedIcon: FontAwesomeIcons.solidUser,
             ),
           ],
         ),
---
+++ lib/features/main_navigation/stf_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class StfScreen extends StatefulWidget {
+  const StfScreen({super.key});
+
+  @override
+  State<StfScreen> createState() => _StfScreenState();
+}
+
+class _StfScreenState extends State<StfScreen> {
+  int _clicks = 0;
+
+  void _increase() {
+    setState(() {
+      _clicks++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$_clicks',
+            style: TextStyle(fontSize: 36),
+          ),
+          TextButton(
+            onPressed: _increase,
+            child: Text('+'),
+          )
+        ],
+      ),
+    );
+  }
+}
